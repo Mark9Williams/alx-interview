093bcc7d5215834f0d414e8a781ae7f200dcba0b
Mark9Williams <mawejjemarkwilliam@gmail.com>
2024-10-08 19:01:58 +0000

    Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    """Determines if all the boxes can be opened"""
+    if len(boxes) == 1:
+        return True
+    keys = [0]
+    for key in keys:
+        for new_key in boxes[key]:
+            if new_key not in keys and new_key < len(boxes):
+                keys.append(new_key)
+    return len(keys) == len(boxes)
---
+++ 0x01-lockboxes/README.md
@@ -0,0 +1,2 @@
+# 0x01. Lockboxes
+The Lockboxes problem is a classic problem in graph theory that can be approached using a traversal strategy.
