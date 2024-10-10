d6029e94fe2a6f139a6fa1c9f956ecd484aa00af
Mark9Williams <mawejjemarkwilliam@gmail.com>
2024-10-10 18:45:49 +0000

    writing_convetion

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,4 +1,7 @@
-# a function implements pascal triangle
+#!/usr/bin/python3
+"""Pascal's Triangle"""
+
+
 def pascal_triangle(n):
     """
     Returns a list of lists of integers representing the Pascal’s triangle of n
